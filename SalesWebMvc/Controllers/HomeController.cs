3710f9a90523cba2c79ee33c6f7f899a615e52fd
Thiago Felix <felix.gjabr@gmail.com>
2022-01-08 15:09:27 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "First controller and Razor pages tests";
+            ViewData["Professor"] = "Nélio Alves";
 
             return View();
         }
